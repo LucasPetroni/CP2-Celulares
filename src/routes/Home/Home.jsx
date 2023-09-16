5b62c5c5a82863befdc7446d1035058269bc95c6
Lucas Petroni <114711407+LucasPetroni@users.noreply.github.com>
2023-09-15 21:59:27 -0300

    imagem

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -13,7 +13,10 @@ export default function Home() {
                 <Link to="/produto/5">
                   <div className="Anun">
                     <h1>Oferta</h1>
-                    <img src="../public/celular5.jpg" alt="foto celular" />
+                    <img
+                      src="https://planoscelular.claro.com.br/medias/18519-0-zero-300Wx300H-productCard?context=bWFzdGVyfGltYWdlc3w5NTI2NXxpbWFnZS9wbmd8YUdVNUwyZzNPUzg1TlRBeU56TXpNamN4TURjd0x6RTROVEU1WHpCZmVtVnliMTh6TURCWGVETXdNRWhmY0hKdlpIVmpkRU5oY21RfDhiYmM4NzFjNzM1YjQ5YWZlN2RhZjQ5OGMwNWU0NmQzNjg2OWQ3NjNhODM5NjNmYWRiNGYyOGZhYjRkN2I0NGM"
+                      alt="foto celular"
+                    />
                     <p>Galaxy S21 Ultra</p>
                   </div>
                 </Link>
@@ -43,7 +46,10 @@ export default function Home() {
                 <Link to="/produto/1">
                   <div className="Anun">
                     <h1>Oferta</h1>
-                    <img src="../public/celular1.png" alt="foto celular" />
+                    <img
+                      src="https://planoscelular.claro.com.br/medias/18297-0-zero-515Wx515H-productMain?context=bWFzdGVyfGltYWdlc3wxNTQyMTV8aW1hZ2UvcG5nfGFHWmtMMmc1TkM4NU5qa3lOamszTWprMk9USTJMekU0TWprM1h6QmZlbVZ5YjE4MU1UVlhlRFV4TlVoZmNISnZaSFZqZEUxaGFXNHxhODdhZDE3ZWVmOTVlYTVlNzQ3ZjQzYWMyN2JhN2Q5MWFhZWEzYTNmOTRkMjgxYjA5NWEzNjA1NjJhNzhlYzkz"
+                      alt="foto celular"
+                    />
                     <p>Iphone 12</p>
                   </div>
                 </Link>
